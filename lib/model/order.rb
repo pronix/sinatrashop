d5abbe6d787fcf9c721ff50a4fcb2c836c2a1e13
Steph Skardal <steph@endpoint.com>
2011-01-14 19:58:45 -0500

    Additional Part 3 work.

--- lib/model/order.rb
+++ lib/model/order.rb
@@ -15,5 +15,5 @@ class Order < ActiveRecord::Base
   validates_presence_of :phone
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 
-  has_one :product
+  belongs_to :product
 end

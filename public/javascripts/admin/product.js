6a9ee37c44590c8bff59c12f089a3ba22d77f999
Steph Skardal <steph@endpoint.com>
2011-01-19 22:09:37 -0500

    JavaScript overhaul to use jQuery.

--- public/javascripts/admin/product.js
+++ public/javascripts/admin/product.js
@@ -1,17 +1,21 @@
 functions.product = {
 	edit: function(product) {
-		return '<label for="name">Name</label>'
-			+ '<input type="text" name="name" value="' + product.name + '" />'
-			+ '<label for="price">Price</label>'
-			+ '<input type="text" name="price" value="' + product.price + '" />'
-			+ '<label for="description">Description</label>'
-			+ '<input type="text" name="description" value="' + product.description + '" />';
+		return '<h4>Editing Product: ' + product.id + '</h4>'
+			+ '<p><label for="name">Name</label>'
+			+ '<input type="text" name="name" value="' + product.name + '" /></p>'
+			+ '<p><label for="price">Price</label>'
+			+ '<input type="text" name="price" value="' + parseFloat(product.price).toFixed(2) + '" /></p>'
+			+ '<p><label for="description">Description</label>'
+			+ '<textarea name="description">' + product.description + '</textarea></p>';
 	},
 	content: function(product) {
-		var inner_html = product.name + ' ' + product.price + ' ' + product.description;
+		var inner_html = '<h4>Product: ' + product.id + '</h4>'
+			+ 'Name: ' + product.name
+			+ '<br />Price: $' + parseFloat(product.price).toFixed(2)
+			+ '<br />Description: ' + product.description + '<br />';
 		return inner_html;
 	},
 	empty: function() {
-		return { name: '', price: '', description: '' };	
+		return { name: '', price: 0, description: '' };	
 	}
 };
